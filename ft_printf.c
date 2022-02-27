348cfd930bc39adc825f4558b794fd61196aea73
Picalexdev <picalex380@gmail.com>
2022-02-27 18:32:26 +0100

    hexa

--- ft_printf.c
+++ ft_printf.c
@@ -12,6 +12,30 @@
 
 #include "ft_printf.h"
 
+size_t	nbr_hexa(size_t nbr)
+{
+	int x;
+	int pos;
+	char a;
+	int y;
+
+	pos = 0;
+	x = 1;
+	while (nbr / (x * 16))
+		x++;
+	while (x >= 1)
+	{
+		printf("x = %d\nnum = %lu", x, nbr % (x * 16));
+		a = 0;
+		if (nbr % (x * 16) < 10)
+			a = '0' + (nbr % (x * 16));
+		else if (nbr % (x * 16) < 16)
+			a = 'A' - 10 + (nbr % (x * 16));
+		write (1, &a, sizeof(char));
+		x = x / 16;
+	}
+}
+
 static size_t	stringfunct(char *c)
 {
 	static size_t	check;
@@ -44,12 +68,14 @@ int	main(int argc, char *argv[])
 	size_t	count;
 
 	n = '\n';
-	if (argc < 2)
+	nbr_hexa(atoi(argv[1]));
+	printf("\n");
+	/*if (argc < 2)
 	{
 		printf("0\n");
 		return (0);
-	}
-	pointfunct(argv[1]);
+	}*/
+	//pointfunct(argv[1]);
 	//count = stringfunct(argv[1]);
 	//printf("\n%u\n", (unsigned int)count);
 	return (0);
